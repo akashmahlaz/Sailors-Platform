aea25ce3590dd405fe343c5ec5167bdcceb9e2af
akashmahlaz <akashdalla408@gmail.com>
2026-06-30 18:17:21 +0530

    db updated

--- lib/mongodb-server.ts
+++ lib/mongodb-server.ts
@@ -1,4 +1,4 @@
-import { MongoClient, ServerApiVersion } from "mongodb"
+import { MongoClient } from "mongodb"
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"')
@@ -6,11 +6,11 @@ if (!process.env.MONGODB_URI) {
 
 const uri = process.env.MONGODB_URI
 const options = {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
+  maxPoolSize: 10,
+  minPoolSize: 1,
+  retryWrites: true,
+  serverSelectionTimeoutMS: 30000,
+  socketTimeoutMS: 45000,
 }
 
 let client
